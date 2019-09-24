05ca4d24db4982362eb85c794f8734f32cccc456
masaaki008 <masaaki.nakamura.tech@gmail.com>
2019-09-24 15:27:04 +0900

    src

---
+++ src/Screen01/Hello.java
@@ -0,0 +1,8 @@
+package Screen01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("new Java program");
+        System.out.println("output screen");
+    }
+}
