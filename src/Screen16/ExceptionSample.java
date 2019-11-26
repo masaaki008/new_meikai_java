8868349ab0679f855d982007150287b10423276c
Masaaki008 <masaaki.nakamura.tech@gmail.com>
2019-11-27 00:39:28 +0900

    new

---
+++ src/Screen16/ExceptionSample.java
@@ -0,0 +1,43 @@
+package Screen16;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionSample {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("xとyの加減乗除します");
+
+        while (true) {
+            try {
+                System.out.print("xの値:");
+                int x = stdIn.nextInt();
+
+                System.out.print("yの値:");
+                int y = stdIn.nextInt();
+
+                System.out.println("x + y = " + (x + y));
+                System.out.println("x - y = " + (x - y));
+                System.out.println("x * y = " + (x * y));
+                System.out.println("x / y = " + (x / y));
+                System.out.println("x % y = " + (x % y));
+            } catch (InputMismatchException e) {
+                System.out.println("入力エラー発生: " + e);
+                String s = stdIn.next();
+                System.out.println(s + "は無視しました");
+            } catch (ArithmeticException e) {
+                System.out.println("算術エラー発生: " + e);
+                System.out.println("エラーが出ないような数値をお願いします");
+            } finally {
+                System.out.println("-----------------------");
+                System.out.println("もう一度?(1.Yes/0.No) : ");
+                int retry = stdIn.nextInt();
+                if (retry == 0) {
+                    break;
+                }
+                System.out.println("-----------------------");
+            }
+        }
+    }
+}
